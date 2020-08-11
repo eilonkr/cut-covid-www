d27345ebd85f2fbcd74e41244cc5933b29c7d050
mac <mac@ret15retn.local>
2020-08-11 20:49:06 +0300

    WIP: Moving files around
    trying to come up with a more sensible directory structure. for now the
    pwa is not working :-(

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+export default [{
+  input: 'src/pwa.js',
+  output: {
+    file: 'dist/pwa.js',
+    format: 'iife'
+  }
+} /*, {
+  input: 'src/cico.js',
+  output: {
+    file: 'dist/cico.js',
+    format: 'iife'
+  }
+} */ ]
